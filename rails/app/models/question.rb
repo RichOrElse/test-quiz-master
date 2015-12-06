a6c030df1b27ea2ec828ccc9934a9fb989af72cb
Ritchie Paul Buitre <ritchie@richorelse.com>
2015-12-06 07:51:36 +0000

    Question#is_correct? understand numbers as words

--- rails/app/models/question.rb
+++ rails/app/models/question.rb
@@ -4,7 +4,22 @@ class Question < ActiveRecord::Base
   validates_presence_of :answer
 
   def is_correct?(submission)
-    answer.downcase.split == submission.downcase.split
+    if is_number?(submission)
+      words_for(submission.to_i.to_words) == words_for(answer)
+    elsif is_number?(answer)
+      words_for(submission) == words_for(answer.to_i.to_words)
+    else
+      words_for(submission) == words_for(answer)
+    end
   end
-
+  
+  private
+  
+  def words_for(text)
+    text.downcase.split
+  end
+  
+  def is_number?(text)
+    text[/\d+/] == text
+  end
 end

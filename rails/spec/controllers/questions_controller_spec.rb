9cbf2fbbd880bfcc60028a7cf24a5cccf4185521
Ritchie Paul Buitre <ritchie@richorelse.com>
2015-12-06 16:50:26 +0000

    test questions#answer

--- rails/spec/controllers/questions_controller_spec.rb
+++ rails/spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
-  it 'is missing specs'
+  describe "POST #answer" do
+    subject { post :answer, id: 1, answer: { answer: "correct" } }
+
+    let(:question)     { double("question") }
+
+    before do
+      allow(Question).to receive(:find) { question }
+    end
+
+    context "when answer is correct" do
+      it "renders correct message" do
+        expect(question).to receive(:is_correct?) { true }
+        expect(subject.body).to include("Correct")
+      end
+    end
+
+    context "when answer is wrong" do
+      it "renders wrong message" do
+        expect(question).to receive(:is_correct?) { false }
+        expect(subject.body).to include("Wrong")
+      end
+    end
+  end
 end
+

f068267896def7cc46c1791b164639448b5c0926
Ritchie Paul Buitre <ritchie@richorelse.com>
2015-12-09 17:04:38 +0800

    sanitize MarkdownPresenter methods #to_html & #to_text

--- rails/app/presenters/markdown_presenter.rb
+++ rails/app/presenters/markdown_presenter.rb
@@ -1,14 +1,26 @@
-class MarkdownPresenter < Struct.new(:to_s)
+class MarkdownPresenter
   require 'redcarpet/render_strip'
 
   TEXT_RENDERER = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
   HTML_RENDERER = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-  
+
+  attr_reader :to_s, :view
+
+  def initialize to_s, view = NullSanitizer.new
+  	@to_s, @view = to_s, view
+  end
+ 
   def to_html
-    HTML_RENDERER.render to_s
+    view.sanitize(HTML_RENDERER.render to_s)
   end
 
   def to_text
-    TEXT_RENDERER.render to_s
+    view.sanitize(TEXT_RENDERER.render to_s)
+  end
+
+  class NullSanitizer
+  	def sanitize(text)
+  	  text
+  	end
   end
 end

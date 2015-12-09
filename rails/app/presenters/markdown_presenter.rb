077686e3f3b45d36812f4bcac1d6cb63624eff44
Ritchie Paul Buitre <ritchie@richorelse.com>
2015-12-09 17:30:22 +0800

    refactor MarkdownPresenter: fix white space & remove view attribute

--- rails/app/presenters/markdown_presenter.rb
+++ rails/app/presenters/markdown_presenter.rb
@@ -4,23 +4,25 @@ class MarkdownPresenter
   TEXT_RENDERER = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
   HTML_RENDERER = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
 
-  attr_reader :to_s, :view
-
-  def initialize to_s, view = NullSanitizer.new
-  	@to_s, @view = to_s, view
+  def initialize markdown, sanitizer = NullSanitizer.new
+    @markdown, @view = markdown, sanitizer
   end
  
   def to_html
-    view.sanitize(HTML_RENDERER.render to_s)
+    @view.sanitize(HTML_RENDERER.render @markdown)
   end
 
   def to_text
-    view.sanitize(TEXT_RENDERER.render to_s)
+    @view.sanitize(TEXT_RENDERER.render @markdown)
+  end
+
+  def to_s
+  	@markdown
   end
 
   class NullSanitizer
-  	def sanitize(text)
-  	  text
-  	end
+    def sanitize(text)
+      text
+    end
   end
 end
